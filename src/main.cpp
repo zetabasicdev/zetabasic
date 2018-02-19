e1ba726706639ecf2956ba7e17d76da264d82a71
Jason <support@zetabasic.com>
2018-02-19 05:13:18 -0600

    catch compiler errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <cstdio>
+#endif
+
+#include <sstream>
+
 #include "CompileError.h"
 #include "Compiler.h"
 #include "TextSourceStream.h"
 
 int main(int argc, char* argv[])
 {
-    const char* code = "END";
+    try {
+        const char* code = "EN$";
 
-    Compiler compiler;
-    TextSourceStream stream(code);
+        Compiler compiler;
+        TextSourceStream stream(code);
 
-    compiler.run(stream);
+        compiler.run(stream);
+    }
+    catch (const CompileError& err) {
+        std::stringstream msg;
+        msg << "[" << err.getRange().getStartRow() << ":" << err.getRange().getStartCol() << "] " << err.what();
+#ifdef _WIN32
+        (void)MessageBoxA(nullptr, msg.str().c_str(), "Compile Error", MB_OK|MB_ICONERROR);
+#else
+        fprintf(stderr, "%s\n", msg.str().c_str());
+#endif
+        return -1;
+    }
 
     return 0;
 }
